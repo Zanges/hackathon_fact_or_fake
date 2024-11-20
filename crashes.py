81f3c91ef9b59a495e841b046660d5033fea7e8f
Zanges <3868054+Zanges@users.noreply.github.com>
2024-11-20 15:55:07 +0100

    crashes.py cleanup

--- crashes.py
+++ crashes.py
@@ -1,12 +1,11 @@
-import random
-
 from pretty_print import get_player_answer
+from utils import replace_nth_word
 
 YES_NO_CHOICES = ["yes", "no"]
 
 
 def wants_crash() -> bool:
-    """Ask User for to call a crash"""
+    """Ask User if they want to use the ultimate crash"""
     use_crash = get_player_answer(
         "Do you want to use the ultimate crash? (yes/no): ", YES_NO_CHOICES
     )
@@ -15,31 +14,19 @@ def wants_crash() -> bool:
     return False
 
 
-def choose_crash():
+def choose_crash() -> callable:
     """Ask User to choose crash"""
     crash_choices = [crash["label"] for crash in CRASH_LIST]
     crash_choice = get_player_answer("Choose a crash: ", crash_choices)
     return CRASH_LIST[crash_choice]["function"]
 
 
-def replace_nth_word(text, n, word_1, word_2) -> str:
-    """Replace nth word with word_1 and every n*2th word with word_2"""
-    words = text.split()
-    word_count = len(words)
-    for i in range(word_count):
-        if i % n == 0:
-            words[i] = word_1
-        if i % n * 2 == 0:
-            words[i] = word_2
-    return " ".join(words)
-
-
-def all_o_crash(text) -> str:
+def all_o_crash(text: str) -> str:
     """Crash to replace all e,u,i,a's with o's"""
     return text.replace("e", "o").replace("u", "o").replace("i", "o").replace("a", "o")
 
 
-def lower_upper_crash(text) -> str:
+def lower_upper_crash(text: str) -> str:
     """Crash to flip upper case and lower case"""
     return "".join([
         letter.lower() if letter != letter.lower() else letter.upper()
@@ -47,17 +34,17 @@ def lower_upper_crash(text) -> str:
     ])
 
 
-def dogs_and_cats_crash(text) -> str:
+def dogs_and_cats_crash(text: str) -> str:
     """Don't eat the cats and dogs!"""
     return replace_nth_word(text, 4, "dog", "cat")
 
 
-def eeeh_crash(text) -> str:
+def eeeh_crash(text: str) -> str:
     """Crash to replace every 5th word with eeeh or 10th word with mhhh"""
     return replace_nth_word(text, 5, "eeeh", "mhhh")
 
 
-def hide_crash(text) -> str:
+def hide_crash(text: str) -> str:
     """Crash to replace every 3rd word with ***** or ####"""
     return replace_nth_word(text, 3, "*****", "####")
 
--- utils.py
+++ utils.py
@@ -7,3 +7,15 @@ def clear_console():
         os.system("cls")
     else:  # macOS/Linux
         os.system("clear")
+
+
+def replace_nth_word(text: str, n, word_1: str, word_2: str) -> str:
+    """Replace nth word with word_1 and every n*2th word with word_2"""
+    words = text.split()
+    word_count = len(words)
+    for i in range(word_count):
+        if i % n == 0:
+            words[i] = word_1
+        if i % n * 2 == 0:
+            words[i] = word_2
+    return " ".join(words)
