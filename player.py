ce80be0fa9d9e07ddb3809a703d1ec53b2b7c524
Zanges <3868054+Zanges@users.noreply.github.com>
2024-11-20 16:05:25 +0100

    player.py cleanup

--- player.py
+++ player.py
@@ -8,19 +8,6 @@ class Player:
     COLORS = (Fore.RED, Fore.GREEN, Fore.BLUE, Fore.CYAN, Fore.MAGENTA)
     color_index = 0
 
-    def __init__(self, name) -> None:
-        self.name = name
-        self.color = self.get_color()
-        self.score = 0
-        self.has_crash = True
-
-    def update_score(self, score) -> None:
-        if not score:
-            self.scored_in_a_row = 0
-        else:
-            self.score += score
-
-
     @classmethod
     def get_color(cls) -> str:
         # Get the current color based on the index
@@ -29,13 +16,18 @@ class Player:
         cls.color_index = (cls.color_index + 1) % len(cls.COLORS)
         return color
 
+    def __init__(self, name) -> None:
+        self.name = name
+        self.color = self.get_color()
+        self.score = 0
+        self.scored_in_a_row = 0
+        self.has_crash = True
+
+    def update_score(self, score) -> None:
+        if not score:
+            self.scored_in_a_row = 0
+        else:
+            self.score += score
+
     def __repr__(self) -> str:
         return f"Player(name={self.name}, color={self.color}, score={self.score})"
-
-
-# player_1 = Player("Phil")
-# print(player_1)
-# player_2 = Player("Paul")
-# print(player_2)
-# player_3 = Player("Lea")
-# print(player_3)
